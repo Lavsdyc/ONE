dac5fbf08d8f869f330c60ad5f43041e6d1f6aca
Lavsdyc <lavsdyc@hotmail.com>
2025-08-05 20:10:55 -0600

    reiniciando el juego
    Aprendimos cómo crear un programa para verificar si el 'intento' ingresado es igual al número secreto definido.Utilizamos estructuras condicionales para tomar decisiones basadas en el resultado de esta comparación;
    Creamos una variable para almacenar la cantidad de intentos realizados por el usuario;
    Hemos visto la importancia de consultar la documentación del lenguaje y las bibliotecas utilizadas en el desarrollo del programa. La documentación es una fuente valiosa de información que nos ayuda a comprender conceptos y a utilizar correctamente las funcionalidades disponibles.

--- 2035-logica-programacion-2-ProyectoInicial/app.js
+++ 2035-logica-programacion-2-ProyectoInicial/app.js
@@ -1,32 +1,59 @@
 let numeroMaximo = 10;
-let numeroSecreto = generarNumeroSecreto();
+let numeroSecreto = 0;
+let intentos = 1;
 
-//console.log(numeroSecreto);
 
 function asignarTextoEtiqueta(etiqueta,texto){
     let etiquetaHTML = document.querySelector(etiqueta);
     etiquetaHTML.innerHTML = texto;
     return;
-
 }
-
-asignarTextoEtiqueta('h1','Juego del número secreto');
-
-asignarTextoEtiqueta('p',`Ingresa un número del 1 al ${numeroMaximo}`);
-
-function generarNumeroSecreto(){
-   return Math.floor(Math.random()*numeroMaximo)+1;  
-}
-
-/* Eso se reduce gracias a usar la función:
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Juego del núnero secreto"
-
-let texto = document.querySelector('p');
-texto.innerHTML = `Ingresa un número del 1 al ${numeroMaximo}`;*/
 
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('numeroUsuario').value);
-    console.log(numeroDeUsuario)
+
+    if(numeroDeUsuario === numeroSecreto){
+        asignarTextoEtiqueta('p',`Acertaste el número secreto en ${intentos} ${(intentos === 1)? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (numeroDeUsuario > numeroSecreto){
+            asignarTextoEtiqueta('p',`El número secreto es menor`);
+        } else {
+             asignarTextoEtiqueta('p',`El número secreto es mayor`);
+        }
+        intentos++;
+        limpiarCaja();
+    }
     return;
+}
+
+function limpiarCaja(){
+    document.querySelector('#numeroUsuario').value = "";
+}
+
+function condicionesIniciales (){
+    asignarTextoEtiqueta('h1','Juego del número secreto');
+    asignarTextoEtiqueta('p',`Ingresa un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    console.log(numeroSecreto);
+    intentos = 1;
+}
+
+function reiniciarJuego (){
+    //Cada vez que se reinicie el juego debe:
+    //    1.Limpiar la caja
+    limpiarCaja();
+    //    2. indicar mensaje de intervalo de numeros
+    //    3. generar numero aleatorio
+    //   4. Reiniciar el numero de intentos 
+    condicionesIniciales();
+    //   5. Desahibilitar el boton de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+}
+
+condicionesIniciales();
+
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random() * numeroMaximo) + 1;
 }
